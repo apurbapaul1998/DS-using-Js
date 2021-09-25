4e8af0585d549e36ff23d2d0c44cf8d2cf7cae62
Apurba Paul <48132427+apurbapaul1998@users.noreply.github.com>
2021-09-25 22:35:31 +0530

    Add files via upload

---
+++ bigO.js
@@ -0,0 +1,13 @@
+const mini=['mini']
+
+function findMini(array){
+    let t0=performance.now(); //built-in method
+    for(let i=0;i<array.length;i++){
+        if(array[i]==='mini'){
+        console.log("found mini");
+        }
+    }
+    let t1=performance.now();
+    console.log("call to find mini took"+(t1-t0)+"ms")
+}
+findMini(mini)
