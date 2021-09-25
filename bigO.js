2f5ebfd4950c69bfb8054393976ca239fe692ba1
Apurba Paul <48132427+apurbapaul1998@users.noreply.github.com>
2021-09-25 22:44:40 +0530

    Update bigO.js

--- bigO.js
+++ bigO.js
@@ -1,5 +1,5 @@
 const mini=['mini']
-
+//const large= new Array('10000').fill('mini') creating a new array and filling 10000 mini in that
 function findMini(array){
     let t0=performance.now(); //built-in method
     for(let i=0;i<array.length;i++){
@@ -11,5 +11,7 @@ function findMini(array){
     console.log("call to find mini took"+(t1-t0)+"ms")
 }
 findMini(mini)
+//findMini(large) //it's O(n)/ no. of operations here is n 
+//so it' linear here/ no. of operations increasing linearly
 
 //big O allows us and concerns us with how many steps it takes in a function
