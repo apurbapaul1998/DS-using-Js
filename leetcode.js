0c1b28d5892292652507812af3c2d813fe3ef349
Apurba Paul <48132427+apurbapaul1998@users.noreply.github.com>
2021-10-02 19:58:49 +0530

    Update leetcode.js

--- leetcode.js
+++ leetcode.js
@@ -49,3 +49,17 @@ var maxSubArray = function(nums) {
     }
 
 
+
+ //Move Zeroes//
+    var moveZeroes = function(nums) {
+        let i=0;
+        for(let j=0;j<nums.length;j++){
+            if(nums[j]!=0){
+                nums[i]=nums[j];
+                i++
+            }
+        }
+        for(let j=i;j<nums.length;j++)
+            nums[j]=0;
+    };
+
